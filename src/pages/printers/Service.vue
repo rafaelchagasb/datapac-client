15c9fcab4be440c037991530f77a4d20270a4673
Rafael Chagas <rafa15chagas@gmail.com>
2021-03-27 18:34:21 -0300

    description and photos fields

--- src/pages/printers/Service.vue
+++ src/pages/printers/Service.vue
@@ -1,41 +1,64 @@
 <template>
-    <div class="q-pa-md items-start q-gutter-md">
+     <div class="items-start">
+        <div class="q-pa-md  q-gutter-md">
 
-        <q-select
-          filled
-          v-model="tonner"
-          multiple
-          :options="optionsTonner"
-          use-chips
-          stack-label
-          label="Tonner"
-        />
+          <q-select
+            filled
+            v-model="tonner"
+            multiple
+            :options="optionsTonner"
+            use-chips
+            stack-label
+            label="Tonner"
+          />
 
-        <q-select
-          filled
-          v-model="drum"
-          multiple
-          :options="optionsDrum"
-          use-chips
-          stack-label
-          label="Drum"
-        />
-        
-        <q-select
-          filled
-          v-model="trays"
-          multiple
-          :options="optionsTrays"
-          use-chips
-          stack-label
-          label="Trays"
-        />
+          <q-select
+            filled
+            v-model="drum"
+            multiple
+            :options="optionsDrum"
+            use-chips
+            stack-label
+            label="Drum"
+          />
+          
+          <q-select
+            filled
+            v-model="trays"
+            multiple
+            :options="optionsTrays"
+            use-chips
+            stack-label
+            label="Trays"
+          />
 
-        <q-checkbox v-model="tonnerCollection" label="Tonner Collection Unit" />
+          <q-checkbox v-model="tonnerCollection" label="Tonner Collection Unit" />
 
-        <q-checkbox v-model="fuser" label="Fuser" />
-        
-        <q-checkbox v-model="adfRollers" label="ADF Rollers " />
+          <q-checkbox v-model="fuser" label="Fuser" />
+          
+          <q-checkbox v-model="adfRollers" label="ADF Rollers " />
+
+          <q-input
+            label="Description" 
+            v-model="description"
+            outlined
+            type="textarea"
+          />
+          <div class="row">
+            <q-uploader
+              url="http://localhost:4444/upload"
+              label="Photos"
+              multiple
+              batch
+              class="col-12"
+            />
+          </div>
+          
+        </div>
+
+        <q-footer class="shadow-up-5">
+          <q-btn class="col-12 q-pa-md full-width" color="blue" label="Save"  @click="saveService" />
+        </q-footer>
     </div>
 </div>
 </template>
@@ -51,11 +74,17 @@ export default {
           fuser: false,
           adfRollers: false,
           trays: null,
+          description: null,
           optionsTonner: [ 'Black', 'Yellow', 'Cyan', 'Magenta' ],
           optionsDrum: [ 'Black', 'Yellow', 'Cyan', 'Magenta' ],
           optionsTrays: [ 'Tray 1', 'Tray 2', 'Tray 3', 'Tray 4' ]
 
       }
+  },
+  methods: {
+    saveService: function() {
+      this.success("Saved service");
+    }
   }
 };
 </script>
